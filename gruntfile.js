1bad216e84eb09251bbfc956f5a9cd4f9add674e
pmiasek <pmiasek@ce5.pl>
2017-09-21 13:19:54 +0200

    podstawowa struktura

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+	sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.scss'
+        }
+      }
+    },
+	imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif,jpeg}'],
+            dest: 'img/'
+        }]
+    }
+},
+	
+watch: {
+    scripts: {
+        files: ['sass/*.scss'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        }
+    }
+},
+  });
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  
+
+  // Default task(s).
+  grunt.registerTask('default', ["sass", "imagemin", "watch"]);
+};
